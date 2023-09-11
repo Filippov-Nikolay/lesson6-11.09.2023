05c70ab63c60af1b8cd81a4d50d2d4eea8b43b56
Filippov-Nikolay <filippovnikolay195@gmail.com>
2023-09-11 19:02:44 +0300

    Добавьте файлы проекта.

---
+++ lesson6 11.09.2023/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include "Laptop.h"
+
+using namespace std;
+
+int main() {
+    system("chcp 1251");
+    setlocale(0, "");
+
+    cout << "Hello World!\n";
+
+    return 0;
+}
